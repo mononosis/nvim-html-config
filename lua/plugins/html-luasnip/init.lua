3c032d094cbb67d7246bfb5a3f20a5a7b8c350ce
Your Name <you@example.com>
2024-08-21 08:09:29 +0000

    quick push

--- lua/plugins/html-luasnip/init.lua
+++ lua/plugins/html-luasnip/init.lua
@@ -65,40 +65,39 @@ function M.setup()
   vim.api.nvim_create_autocmd("User", {
     pattern = "LuasnipChoiceNodeEnter",
     callback = function()
-      local node = require("luasnip").session.event_node
-      print(vim.inspect(node))
-      vim.defer_fn(function()
-        local choices = ls.get_current_choices()
+      local choices = ls.get_current_choices()
+      if choices then
+        vim.defer_fn(function()
+          -- Use Telescope to select a choice
+          pickers.new({}, {
+            prompt_title = "Choose an option:",
+            finder = finders.new_table {
+              results = choices,
+              entry_maker = function(entry)
+                return {
+                  value = entry,
+                  display = "-> " .. entry,
+                  ordinal = entry,
+                }
+              end,
+            },
+            sorter = sorters.get_generic_fuzzy_sorter(),
+            attach_mappings = function(prompt_bufnr, map)
+              map('i', '<CR>', function()
+                local selection = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
 
-        -- Use Telescope to select a choice
-        pickers.new({}, {
-          prompt_title = "Choose an option:",
-          finder = finders.new_table {
-            results = choices,
-            entry_maker = function(entry)
-              return {
-                value = entry,
-                display = "-> " .. entry,
-                ordinal = entry,
-              }
+                if selection then
+                  ls.set_choice(selection.index)
+                else
+                  print("No option selected")
+                end
+              end)
+              return true
             end,
-          },
-          sorter = sorters.get_generic_fuzzy_sorter(),
-          attach_mappings = function(prompt_bufnr, map)
-            map('i', '<CR>', function()
-              local selection = action_state.get_selected_entry()
-              actions.close(prompt_bufnr)
-
-              if selection then
-                ls.set_choice(selection.index)
-              else
-                print("No option selected")
-              end
-            end)
-            return true
-          end,
-        }):find()
-      end, 1)
+          }):find()
+        end, 1000)
+      end
     end,
   })
 
@@ -106,82 +105,89 @@ function M.setup()
     pattern = "LuasnipInsertNodeEnter",
     callback = function()
       local node = require("luasnip").session.event_node
-      --if not ls.choice_active() then
-      vim.defer_fn(function()
-        local cd = node:get_jump_index()
-        local li = vim.fn.input(vim.inspect(cd), node:get_text()[1])
-        node:set_text({ li })
-        ls.jump(_G.LUASNIP_DIRECTION)
-        if not ls.locally_jumpable(_G.LUASNIP_DIRECTION) then
-          _G.LUASNIP_DIRECTION = 1
-          --ls.exit_out_of_region(node)
-          --vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>o", true, false, true), 'n', false)
-        end
-      end, 10)
-      --end
+      if not ls.choice_active() then
+        vim.defer_fn(function()
+          local cd = node:get_jump_index()
+          local li = vim.fn.input(vim.inspect(cd), node:get_text()[1])
+          node:set_text({ li })
+          ls.jump(_G.LUASNIP_DIRECTION)
+          if not ls.locally_jumpable(_G.LUASNIP_DIRECTION) then
+            _G.LUASNIP_DIRECTION = 1
+            --ls.exit_out_of_region(node)
+            --vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>o", true, false, true), 'n', false)
+          end
+        end, 100)
+      end
     end
   })
 
+  --local api_service_snippet = {
+  --s("newApiCall",
+  --fmt([[
+  --interface {name} {{
+  --id: number;
+  --field1: string;
+  --field2?: string;
+  --{field_key}: {field_val};
+  --}}
+  --interface {name}Response {{
+  --data: {name};
+  --message: string;
+  --}}
+  --interface ErrorResponse {{
+  --message: string;
+  --}}
+
+  --export const get{name}ById = async ({name_lower}Id:number): Promise<{name}Response | ErrorResponse> => {{
+  --try {{
+  --const response = await API.get<{name}Response>(`/{name_kebab_case}s/${{{name_lower}Id}}`);
+  --return response.data;
+  --}} catch (error:any) {{
+  --return {{
+  --message: error.response?.data.message || "An unexpected error occurred"
+  --}}
+  --}}
+  --}};
+  --]], {
+  --name = i(1, "name"),
+  --field_key = i(2, "field_key"),
+  --field_val = i(3, "field_val"),
+  --name_lower = l(l._1:sub(1, 1):lower() .. l._1:sub(2), 1),
+  --name_kebab_case = l(l._1:gsub("(%u)", "-%1"):lower():gsub("^%-", ""), 1)
+  --}, {
+  --repeat_duplicates = true
+  --})
+  --)
+  --}
+
   local api_service_snippet = {
-    s("apiService",
+    s("newApiCall",
       fmt([[
-      interface {name} {{
+    interface {name} {{
+      id: number;
+      field1: string;
+      field2?: string;
+      {field_key}: {field_val};
+    }}
+    interface {name}Response {{
+      data: {name};
+      message: string;
+    }}
+    interface ErrorResponse {{
+      message: string;
+    }}
 
-      }}
-      interface {name}Response {{
-        data: {name};
-        message: string;
-      }}
-      interface ErrorResponse {{
-        message: string;
-      }}
-
-      export const get{name}ById = async ({name_lower}Id:number): Promise<{name}Response> | ErrorResponse> => {{
-
-        try {{
-          const response = await API.get<{name}Response>(`/{kebab}s/${{{name_lower}Id}}`);
-        }}
-      }};
-      ]], {
+    export const {api_call_name} 
+    ]], {
         name = i(1, "name"),
-        name_lower = l(l._1:sub(1, 1):lower() .. l._1:sub(2), 1),
-        kebab = l(l._1:gsub("(%u)", "-%1"):lower():gsub("^%-", ""), 1)
+        field_key = i(2, "field_key"),
+        field_val = i(3, "field_val"),
+        api_call_name =  t"ById{name}lH" ,
       }, {
         repeat_duplicates = true
       })
     )
   }
-  --ls.add_snippets("all", {
-  ---- important! fmt does not return a snippet, it returns a table of nodes.
-  --s("example1", fmt("just an {iNode1}", {
-  --iNode1 = i(1, "example")
-  --})),
-  --s("example2", fmt([[
-  --if {} then
-  --{}
-  --end
-  --]], {
-  ---- i(1) is at nodes[1], i(2) at nodes[2].
-  --i(1, "not now"), i(2, "when")
-  --})),
-  --s("example3", fmt([[
-  --if <> then
-  --<>
-  --end
-  --]], {
-  ---- i(1) is at nodes[1], i(2) at nodes[2].
-  --i(1, "not now"), i(2, "when")
-  --}, {
-  --delimiters = "<>"
-  --})),
-  --s("example4", fmt([[
-  --repeat {a} with the same key {a}
-  --]], {
-  --a = i(1, "this will be repeat")
-  --}, {
-  --repeat_duplicates = true
-  --}))
-  --})
 
   ls.add_snippets("all", api_service_snippet)
 
