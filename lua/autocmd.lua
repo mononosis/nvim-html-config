af60dd04f488a235240b6b256193118fc4670ee2
Your Name <you@example.com>
2024-09-07 09:50:45 +0000

    quick push

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,15 +1,16 @@
 local M = {}
 function M.setup()
   vim.api.nvim_create_autocmd("BufReadPost", {
-    pattern = "*ApiService.tsx", -- Matches any file ending with ApiService.tsx
+    pattern = "*ApiService.ts", -- Matches any file ending with ApiService.tsx
     callback = function()
       local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
       if first_line == "" then
         local lines = {
           "// Auto-generated file",
           "// Add your content here",
-          "import { ApiResponse, ErrorResponse } from './apiTypes';",
-          "import API from './apiClient'; "
+          "import API from '../axiosConfig';",
+          "import { handleApiResponse, ApiResponse } from '../utils/handleApiResponse';",
+          "import { ErrorResponse } from '../utils/apiTypes';"
         }
         vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
       end
